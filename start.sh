93c2d083afdf0614c3d356a3270b03fce4aba4a5
danilerti <danilerti99@gmail.com>
2026-04-05 01:22:52 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
-# Подставляем порт Choreo в конфиг Xray
-sed -i "s/8080/$PORT/g" /etc/xray/config.json
-echo "Starting Xray on port $PORT with gRPC..."
-/usr/bin/xray/xray run -config /etc/xray/config.json
+
+# 1. Копируем конфиг туда, где разрешено изменять файлы
+cp /etc/xray/config.json /tmp/config.json
+
+# 2. Правим порт и UUID во временном файле (если используешь sed)
+# Если тебе нужно просто заменить порт на 8080:
+sed -i "s/8080/g" /tmp/config.json
+
+# 3. Запускаем Xray, указывая на новый путь к конфигу
+/usr/bin/xray/xray -config /tmp/config.json
