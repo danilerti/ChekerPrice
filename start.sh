de7024faf699a1b20e1bdfdcefb85d1c163bb446
danilerti <danilerti99@gmail.com>
2026-04-04 23:42:07 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,4 @@
 #!/bin/sh
-
 export PORT=${PORT:-10000}
-
 sed -i "s/10000/$PORT/g" /etc/xray/config.json
-
-/usr/local/bin/xray -config /etc/xray/config.json
+/usr/bin/xray run -config /etc/xray/config.json
