a09edf9b69dcbe95553566185783cc1f2ff4e2cb
danilerti <danilerti99@gmail.com>
2026-04-05 00:28:53 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-export PORT=${PORT:-10000}
+# Подставляем порт Choreo в конфиг Xray
 sed -i "s/10000/$PORT/g" /etc/xray/config.json
+echo "Starting Xray on port $PORT with gRPC..."
 /usr/bin/xray/xray run -config /etc/xray/config.json
