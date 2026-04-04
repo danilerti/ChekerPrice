afff3b9927ad361dfcfda7a34a8ab22ef7b19f7b
danilerti <danilerti99@gmail.com>
2026-04-05 01:43:40 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
-# Копируем конфиг
-cp /etc/xray/config.json /tmp/config.json
 
-# ВАЖНО: Если в оригинальном config.json уже стоит 8080, 
-# просто удали все строки с sed, чтобы ничего не испортить.
-# Если там 10000, используй это:
-sed -i 's/10000/8080/g' /tmp/config.json
-
-/usr/bin/xray/xray -config /tmp/config.json
+# Просто запускаем Xray, так как конфиг уже правильный
+# Мы используем путь /etc/xray/config.json, так как нам больше не нужно ничего править через sed
+/usr/bin/xray/xray -config /etc/xray/config.json
